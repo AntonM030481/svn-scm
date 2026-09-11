fb33fc73f4157aa3cc6cf7fbeeae5e68115638ab
Anton Makouski <85622715+AntonM030481@users.noreply.github.com>
2026-09-11 13:44:21 +0300

    fix: handle unknown test runner errors

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -12,7 +12,7 @@ async function main() {
       extensionTestsPath
     });
   } catch (err) {
-    console.error(`Failed to run tests: ${err}\n${err.stack}`);
+    console.error("Failed to run tests:", err);
     process.exit(1);
   }
 }
