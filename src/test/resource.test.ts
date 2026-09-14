c6b981e6293ec56b80d858ff23ce47b784071d37
Anton Makouski <85622715+AntonM030481@users.noreply.github.com>
2026-09-14 12:42:52 +0300

    test: cover async unversioned directory detection

--- src/test/resource.test.ts
+++ src/test/resource.test.ts
@@ -3,18 +3,36 @@ import * as fs from "node:fs";
 import * as os from "node:os";
 import * as path from "path";
 import { ThemeIcon, Uri } from "vscode";
-import { Status } from "../common/types";
+import { IFileStatus, PropStatus, Status } from "../common/types";
 import { Resource } from "../resource";
+import { refreshUnversionedDirectoryKinds } from "../resourceKinds";
 
 suite("Resource Tests", () => {
-  test("Unversioned directory uses folder decoration", () => {
+  test("Unversioned directory uses folder decoration", async () => {
     const root = fs.mkdtempSync(path.join(os.tmpdir(), "svn-scm-resource-"));
     const directory = path.join(root, "folder");
     const file = path.join(root, "file.txt");
     fs.mkdirSync(directory);
     fs.writeFileSync(file, "test");
 
+    const statuses: IFileStatus[] = [
+      {
+        path: "folder",
+        status: Status.UNVERSIONED,
+        props: PropStatus.NONE,
+        wcStatus: { locked: false, switched: false }
+      },
+      {
+        path: "file.txt",
+        status: Status.UNVERSIONED,
+        props: PropStatus.NONE,
+        wcStatus: { locked: false, switched: false }
+      }
+    ];
+
     try {
+      await refreshUnversionedDirectoryKinds(root, statuses);
+
       const directoryResource = new Resource(
         Uri.file(directory),
         Status.UNVERSIONED
