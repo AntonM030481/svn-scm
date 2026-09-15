7661af8feed2dede097e18c8ca3602185a7a5651
AntonM030481 <a_makovsky@wargaming.net>
2026-09-15 16:23:57 +0300

    Continue shared remote refresh after failures

--- src/commands/refreshRemoteChanges.ts
+++ src/commands/refreshRemoteChanges.ts
@@ -1,4 +1,7 @@
+import * as path from "path";
+import { window } from "vscode";
 import { Repository } from "../repository";
+import { getDisplayErrorMessage } from "../svnError";
 import { Command } from "./command";
 import { workingCopyScopes } from "./workingCopyScopes";
 
@@ -8,8 +11,20 @@ export class RefreshRemoteChanges extends Command {
   }
 
   public async execute(repository: Repository) {
+    const failures: string[] = [];
     for (const scope of await workingCopyScopes(repository)) {
-      await scope.updateRemoteChangedFiles();
+      try {
+        await scope.updateRemoteChangedFiles();
+      } catch (error) {
+        failures.push(
+          `${path.basename(scope.workspaceRoot)}: ${getDisplayErrorMessage(error)}`
+        );
+      }
+    }
+    if (failures.length) {
+      window.showErrorMessage(
+        `Unable to refresh remote changes: ${failures.join("; ")}`
+      );
     }
   }
 }
