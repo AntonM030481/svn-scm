8ed8e4f2b1109b4cf4d6846a1bca262d1a0a1dab
Anton Makouski <85622715+AntonM030481@users.noreply.github.com>
2026-09-19 17:12:11 +0300

    Format path identity tests

--- src/test/pathIdentity.test.ts
+++ src/test/pathIdentity.test.ts
@@ -8,9 +8,7 @@ import { ItemLogProvider } from "../historyView/itemLogProvider";
 import { Repository } from "../repository";
 import { Resource } from "../resource";
 import IncomingChangesNode from "../treeView/nodes/incomingChangesNode";
-import {
-  deduplicateUnversionedResources
-} from "../unversionedDirectoryContents";
+import { deduplicateUnversionedResources } from "../unversionedDirectoryContents";
 import { normalizePath, pathEquals } from "../util";
 import * as testUtil from "./testUtil";
 
