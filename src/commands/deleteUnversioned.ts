d5bce977c5d10df762585fe643c2053a50927f24
Anton Makouski <85622715+AntonM030481@users.noreply.github.com>
2026-09-19 16:31:36 +0300

    Refresh SCM after deleting unversioned files

--- src/commands/deleteUnversioned.ts
+++ src/commands/deleteUnversioned.ts
@@ -6,8 +6,10 @@ import {
   window
 } from "vscode";
 import { Status } from "../common/types";
+import { refreshStatusTargets } from "../incrementalStatus";
 import { exists, lstat, unlink } from "../fs";
 import { Resource } from "../resource";
+import { getDisplayErrorMessage } from "../svnError";
 import { isUnversionedChildResource } from "../unversionedDirectoryContents";
 import { deleteDirectory } from "../util";
 import { Command } from "./command";
@@ -168,24 +170,50 @@ export class DeleteUnversioned extends Command {
       "No"
     );
     if (answer === "Yes") {
-      for (const uri of uris) {
-        const fsPath = uri.fsPath;
+      const failures = (
+        await this.runByRepository(uris, async (repository, resources) => {
+          const deleted: string[] = [];
+          const failed: Array<{ path: string; error: unknown }> = [];
 
-        try {
-          if (!(await exists(fsPath))) {
-            continue;
+          for (const uri of resources) {
+            const fsPath = uri.fsPath;
+
+            try {
+              if (!(await exists(fsPath))) {
+                deleted.push(fsPath);
+                continue;
+              }
+
+              const stat = await lstat(fsPath);
+
+              if (stat.isDirectory()) {
+                await deleteDirectory(fsPath);
+              } else {
+                await unlink(fsPath);
+              }
+              deleted.push(fsPath);
+            } catch (error) {
+              failed.push({ path: fsPath, error });
+            }
           }
 
-          const stat = await lstat(fsPath);
-
-          if (stat.isDirectory()) {
-            await deleteDirectory(fsPath);
-          } else {
-            await unlink(fsPath);
+          if (deleted.length) {
+            await refreshStatusTargets(repository, deleted);
           }
-        } catch (_error) {
-          // TODO(cjohnston) Show meaningful error to user
-        }
+
+          return failed;
+        })
+      ).flat();
+
+      if (failures.length) {
+        const first = failures[0];
+        const suffix =
+          failures.length > 1 ? ` (+${failures.length - 1} more)` : "";
+        window.showErrorMessage(
+          `Unable to delete "${path.basename(first.path)}": ${getDisplayErrorMessage(
+            first.error
+          )}${suffix}`
+        );
       }
     }
   }
